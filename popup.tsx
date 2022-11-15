bbdec62f22b53313e52087851a4e698eb338a7f5
Adil <horhorou@gmail.com>
2022-11-15 14:55:05 +0100

    feat: :sparkles: Initial commit
    - New plasmo project - Added mantine

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,15 @@
+import { Box, MantineProvider, Title } from "@mantine/core"
 import { useState } from "react"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Box>
+        <Title>Popup</Title>
+      </Box>
+    </MantineProvider>
   )
 }
 
